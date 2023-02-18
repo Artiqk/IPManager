1fc8b45058f8930314970197b619fb1b80b028a2
Antoine <antoineabonneau94@gmail.com>
2023-02-18 17:12:10 +0100

    added script to prepare the db

---
+++ create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ip_addresses (
+    id INTEGER PRIMARY KEY,
+    address1 INTEGER NOT NULL,
+    address2 INTEGER NOT NULL,
+    address3 INTEGER NOT NULL,
+    address4 INTEGER NOT NULL
+);
